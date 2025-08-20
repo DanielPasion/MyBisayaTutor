842f1f678d852f4e73d5659ade2d93d57bb9a154
DanielPasion <dnpaxion@gmail.com>
2025-08-19 20:27:26 -0700

    ch

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import Header from "@/components/Header";
 import { colors } from "@/constants/Colors";
 import { Image, Text, View } from "react-native";
 
@@ -6,40 +7,40 @@ export default function Index() {
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
         backgroundColor: colors.cream["500"],
-        display: "flex",
-        flexDirection: "column",
-        gap: 10,
       }}
     >
-      <Image
-        source={require("@/assets/images/logo-transparent.png")}
-        style={{
-          width: 200,
-          height: 200,
-          borderRadius: "30px",
-        }}
-      />
-      <Text
-        style={{
-          fontSize: 40,
-          color: colors.orange["500"],
-          textAlign: "center",
-        }}
-      >
-        My Bisaya Tutor
-      </Text>
-      <Text
-        style={{
-          fontSize: 20,
-          color: colors.green["500"],
-          textAlign: "center",
-        }}
-      >
-        Your Personal, 24/7 Bisaya Tutor
-      </Text>
+      <Header />
+      <View style={{ flex: 3, backgroundColor: colors.cream["500"], gap: 10 }}>
+        <Image
+          source={require("@/assets/images/logo-transparent.png")}
+          style={{
+            width: 200,
+            height: 200,
+            borderRadius: "30px",
+            marginHorizontal: "auto",
+            marginTop: 50,
+          }}
+        />
+        <Text
+          style={{
+            fontSize: 40,
+            color: colors.orange["500"],
+            textAlign: "center",
+          }}
+        >
+          My Bisaya Tutor
+        </Text>
+        <Text
+          style={{
+            fontSize: 20,
+            color: colors.green["500"],
+            textAlign: "center",
+          }}
+        >
+          Your Personal, 24/7 Bisaya Tutor
+        </Text>
+      </View>
     </View>
   );
 }
