f199c1d26afc9a43df02388a67eaaf640e8cf130
DanielPasion <dnpaxion@gmail.com>
2025-08-26 23:15:24 -0700

    ch

--- utils/say-out-loud.ts
+++ utils/say-out-loud.ts
@@ -3,8 +3,23 @@ import * as FileSystem from "expo-file-system";
 import { Platform } from "react-native";
 import { uri } from "./uri";
 
+let lastText: string | null = null;
+let lastFileUri: string | null = null;
+
 export const sayOutLoud = async (text: string) => {
+  console.log(lastText, text);
   try {
+    if (text === lastText && lastFileUri) {
+      if (Platform.OS === "web") {
+        const audio = new Audio(lastFileUri);
+        await audio.play();
+      } else {
+        const player = createAudioPlayer(lastFileUri);
+        player.play();
+      }
+      return;
+    }
+
     const response = await fetch(`${uri}/tts`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -15,12 +30,14 @@ export const sayOutLoud = async (text: string) => {
       const arrayBuffer = await response.arrayBuffer();
       const blob = new Blob([arrayBuffer], { type: "audio/mpeg" });
       const url = URL.createObjectURL(blob);
+      lastText = text;
+      lastFileUri = url;
       const audio = new Audio(url);
       await audio.play();
     } else {
       const blob = await response.blob();
-
       const reader = new FileReader();
+
       reader.onloadend = async () => {
         const base64String = (reader.result as string).split(",")[1]; // remove data:... prefix
         const fileUri = FileSystem.cacheDirectory + "tts.mp3";
@@ -28,6 +45,9 @@ export const sayOutLoud = async (text: string) => {
         await FileSystem.writeAsStringAsync(fileUri, base64String, {
           encoding: FileSystem.EncodingType.Base64,
         });
+
+        lastText = text;
+        lastFileUri = fileUri;
 
         const player = createAudioPlayer(fileUri);
         player.play();
